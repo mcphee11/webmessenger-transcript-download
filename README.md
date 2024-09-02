f2a368182fcdfe75cf9c6bff32d02b8754e9ce85
mcphee11 <mcphee11@gmail.com>
2024-09-02 13:35:37 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # webmessenger-transcript-download
-Ability for customer to download a copy of the webmessenger transcript as a PDF.
+
+This repo is an example of how you can enable the customer to download a copy of the WebMessenger transcript to a `PDF` on their client side. This adds a `download` button next to the default position of the launcher button. If you require to change the color or this button you can do this via the config, to change the location of it you will need to update the code.
+
+![](/docs/images/download.png?raw=true)
+
+Once you download the transcript you will get a PDF like the below screen shot:
+
+![](/docs/images/pdf.png?raw=true)
+
+### types currently supported
+
+Not ALL object types are supported yet fully the below items are supported as well as in what state:
+
+- `Strings` or `Text`
+- `Images` png or jpg/jpeg
+- `Markdown` currently renders in "plain text"
+- `emojis` currently don't render only show square placeholder
+
+### updates planned
+
+- move utc iso time to local
+- render markdown better
+- render emojis better
+- allow more file types as icons only
+
+If anyone whats to help with these new features I'm more then open to a PR for review, otherwise I will get to these if and when i can 😀
+
+## Step 1 - Deploy code
+
+To do this I would recommend using the tag manager that you used to deploy the default WebMessenger deployment code snippet. Download the [genesysTranscript.min.js](./genesysSurvey.min.js) and then host it somewhere, this can be with the rest of your website or a public AWS S3 or GCP Bucket for example. Once you have the URL add it to your website like the below.
+
+You will also need to ensure you put the `configuration` items you want above the .min.js file to ensure the load before the fine runs. Ensure you update the `ENTER_YOUR_DEPLOYMENT_ID` with your own WebMessenger DeploymentId as well as the `region` to your own, as im in Australia its mypurecloud.com.au but your region will depend on the environment your using. The `gc_hexColor` is the color code of the new bubble button, the `gc_iconColor` can either be `white` or `black` and is the SVG icon on the button itself. If your using a lighter hex color I would suggest making the SVG black to ensure it can be seen correctly.
+
+```
+<script>
+const gc_deploymentId = 'ENTER_YOUR_DEPLOYMENT_ID'
+const gc_region = 'mypurecloud.com.au'
+const gc_hexColor = '#000000'
+const gc_iconColor = 'white'
+</script>
+
+<script src=./genesysTranscript.min.js></script>
+```
+
+Ensure that this is `BELOW` the default Genesys Cloud WebMessenger deployment snippet as the `Genesys` SDK that is used int his file needs to be loaded first. If you do run into load issues you can always add a `defer` to this so its forced to load later.
+
+file that is in the repo, while you can use the .js version I have created a .min.js version to compress and mangle the code for prod deployment. I use [terser](https://terser.org/) to do this there are many other packages out there but if you do your own edits you can then use terser to create your own .min version of your own file if you did change the code. Or you can just use your raw .js version of the file.
---
+++ genesysTranscript.js
@@ -0,0 +1,343 @@
+'use strict'
+
+let gc_socket
+let gc_token
+let gc_icon
+
+Genesys('subscribe', 'Launcher.ready', function () {
+  gc_token = JSON.parse(localStorage.getItem(`_${gc_deploymentId}:actmu`)).value
+  displayButton()
+})
+
+function setupWSS() {
+  //Create websocket for events
+  try {
+    gc_socket = new WebSocket(`wss://webmessaging.${gc_region}/v1?deploymentId=${gc_deploymentId}`)
+
+    gc_socket.onmessage = async function (event) {
+      let details = JSON.parse(event.data)
+      console.log(details)
+
+      // get jwt on session started
+      if (details.class === 'SessionResponse') {
+        let json = {
+          action: 'getJwt',
+          token: gc_token,
+        }
+        gc_socket.send(JSON.stringify(json))
+      }
+
+      // get jwt
+      if (details.class === 'JwtResponse') {
+        getHistory(details.body.jwt)
+      }
+    }
+    console.log(`Waiting for events on wss://webmessaging.${gc_region}/v1?deploymentId=${gc_deploymentId}`)
+
+    gc_socket.onopen = function () {
+      let json = {
+        action: 'configureSession',
+        deploymentId: `${gc_deploymentId}`,
+        token: gc_token,
+      }
+      gc_socket.send(JSON.stringify(json))
+    }
+  } catch (err) {
+    console.error('Websocket error: ', err)
+  }
+}
+
+async function getHistory(gc_jwt) {
+  let request = await fetch(`https://api.${gc_region}/api/v2/webmessaging/messages`, {
+    headers: {
+      Authorization: `Bearer ${gc_jwt}`,
+    },
+  })
+  let response = await request.json()
+  console.log(response)
+  createPdf(response)
+}
+
+async function createPdf(history) {
+  const url = 'https://pdf-lib.js.org/assets/ubuntu/Ubuntu-R.ttf'
+  const fontBytes = await fetch(url).then((res) => res.arrayBuffer())
+
+  const pdfDoc = await PDFLib.PDFDocument.create()
+  pdfDoc.registerFontkit(fontkit)
+  const ubuntuFont = await pdfDoc.embedFont(fontBytes)
+
+  let page = pdfDoc.addPage()
+  const { width, height } = page.getSize()
+  const fontSize = 12
+  let maxImageHeight = 150
+  let yPosition = 3
+
+  //loop through history array, reverse for public API
+  for (const msg of history.entities.reverse()) {
+    let image
+    let imageScale = 1.0
+
+    //check for image first
+    if (msg?.content) {
+      if (msg.content[0].contentType == 'Attachment') {
+        if (msg.content[0].attachment.mediaType == 'Image' && msg.content[0].attachment.mime.includes('jpeg')) {
+          const imageBytes = await fetch(msg.content[0].attachment.url).then((res) => res.arrayBuffer())
+          image = await pdfDoc.embedJpg(imageBytes)
+          if (image.height > maxImageHeight) {
+            imageScale = maxImageHeight / image.height
+          }
+        }
+        if (msg.content[0].attachment.mediaType == 'Image' && msg.content[0].attachment.mime.includes('png')) {
+          const imageBytes = await fetch(msg.content[0].attachment.url).then((res) => res.arrayBuffer())
+          image = await pdfDoc.embedPng(imageBytes)
+          if (image.height > maxImageHeight) {
+            imageScale = maxImageHeight / image.height
+          }
+        }
+      }
+    }
+
+    if (msg.text) {
+      // Check height incase new page is needed
+      let newPage = newPageNeeded_rec_txt(page, ubuntuFont, msg.text, yPosition, width, height, fontSize)
+      if (newPage.value) {
+        page = pdfDoc.addPage()
+        yPosition = 3
+      }
+      if (msg.direction == 'Outbound') {
+        // Genesys
+        const next = agentText(page, ubuntuFont, msg.text, yPosition, width, height, fontSize, msg.channel.time)
+        yPosition = next
+      }
+      if (msg.direction == 'Inbound') {
+        // Customer
+        const next = customerText(page, ubuntuFont, msg.text, yPosition, width, height, fontSize, msg.channel.time)
+        yPosition = next
+      }
+    }
+    if (image) {
+      let newPage = newPageNeeded_rec_img(page, yPosition, height, image.scale(imageScale).height, fontSize)
+      if (newPage) {
+        page = pdfDoc.addPage()
+        yPosition = 3
+      }
+      const next = customerImage(page, ubuntuFont, image, imageScale, yPosition, width, height, fontSize, msg.channel.time)
+      yPosition = next
+    }
+  }
+
+  pdfDoc.setCreationDate(new Date())
+  pdfDoc.setAuthor('https://github.com/mcphee11')
+  pdfDoc.setSubject('Messaging transcript')
+  const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true })
+
+  const downloadLink = document.createElement('a')
+  downloadLink.href = pdfDataUri
+  downloadLink.download = 'transcript.pdf'
+  downloadLink.click()
+
+  loadingOff()
+}
+
+function customerText(page, font, text, start, width, height, fontSize, time) {
+  let second = start + 2
+  let third = second + 1
+
+  let newPage = newPageNeeded_rec_txt(page, font, text, start, width, height, fontSize)
+
+  page.drawText('Customer', {
+    x: 20,
+    y: height - start * fontSize,
+    size: fontSize,
+    font: font,
+  })
+
+  let opts = {
+    x: 20,
+    y: height - second * fontSize,
+    maxWidth: width - 40,
+    wordBreaks: [' '],
+    font: font,
+    size: fontSize,
+    lineHeight: fontSize,
+  }
+
+  page.drawRectangle({
+    x: 10,
+    y: height - third * fontSize - newPage.rec.height + fontSize,
+    height: newPage.rec.height + 20,
+    width: newPage.rec.width + 20,
+    color: PDFLib.rgb(0, 0.8, 1),
+  })
+  page.drawText(text, opts)
+  let forth = third + newPage.rec.lineCount
+
+  page.drawText(time, {
+    x: 20,
+    y: height - forth * fontSize,
+    size: 8,
+    font: font,
+  })
+
+  let next = forth + 1
+
+  return next
+}
+
+function customerImage(page, font, image, imageScale, start, width, height, fontSize, time) {
+  let second = start + 2
+  let third = second + 1
+
+  page.drawText('Customer', {
+    x: 20,
+    y: height - start * fontSize,
+    size: fontSize,
+    font: font,
+  })
+
+  page.drawImage(image, {
+    x: 20,
+    y: height - third * fontSize - image.scale(imageScale).height,
+    width: image.scale(imageScale).width,
+    height: image.scale(imageScale).height,
+  })
+  let forth = third + image.scale(imageScale).height / fontSize + 1
+
+  page.drawText(time, {
+    x: 20,
+    y: height - forth * fontSize,
+    size: 8,
+    font: font,
+  })
+
+  let next = forth + 1
+
+  return next
+}
+
+function agentText(page, font, text, start, width, height, fontSize, time) {
+  let second = start + 2
+  let third = second + 1
+
+  let newPage = newPageNeeded_rec_txt(page, font, text, start, width, height, fontSize)
+
+  page.drawText('Genesys', {
+    x: width - 70,
+    y: height - start * fontSize,
+    size: fontSize,
+    font: font,
+  })
+
+  let opts = {
+    x: width - newPage.rec.width - 20,
+    y: height - second * fontSize,
+    maxWidth: width - 40,
+    wordBreaks: [' '],
+    font: font,
+    size: fontSize,
+    lineHeight: fontSize,
+  }
+
+  page.drawRectangle({
+    x: width - newPage.rec.width - 30,
+    y: height - third * fontSize - newPage.rec.height + fontSize,
+    height: newPage.rec.height + 20,
+    width: newPage.rec.width + 20,
+    color: PDFLib.rgb(0.8, 0.6, 0),
+  })
+  page.drawText(text, opts)
+  let forth = third + newPage.rec.lineCount
+
+  page.drawText(time, {
+    x: width - 120,
+    y: height - forth * fontSize,
+    size: 8,
+    font: font,
+  })
+
+  let next = forth + 1
+
+  return next
+}
+
+function newPageNeeded_rec_img(page, start, height, imageHeight, fontSize) {
+  let second = start + 2
+  let third = second + 1
+
+  let value = false
+  let bottom = height - third * fontSize - imageHeight + fontSize
+  if (bottom < 5) {
+    value = true
+  }
+  return value
+}
+
+function newPageNeeded_rec_txt(page, font, text, start, width, height, fontSize) {
+  let second = start + 2
+  let third = second + 1
+  let value = false
+
+  let build = {
+    maxWidth: width - 40,
+    wordBreaks: [' '],
+    font: font,
+    size: fontSize,
+    lineHeight: fontSize,
+  }
+
+  let rec = drawMultilineText(page, text, build)
+  let bottom = height - third * fontSize - rec.height + fontSize
+
+  if (bottom < 5) {
+    value = true
+  }
+
+  return { value, rec }
+}
+
+function drawMultilineText(page, text, opts) {
+  const lines = PDFLib.breakTextIntoLines(text, opts.wordBreaks || page.doc.defaultWordBreaks, opts.maxWidth, (t) => opts.font.widthOfTextAtSize(t, opts.size))
+  const lineCount = lines.length
+  const height = lineCount * opts.lineHeight
+  const width = Math.max(...lines.map((l) => opts.font.widthOfTextAtSize(l, opts.size)))
+  return { width, height, lineCount }
+}
+
+const downLoadSvgBlack = `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><rect fill="none" height="24" width="24"/></g><g><path d="M5,20h14v-2H5V20z M19,9h-4V3H9v6H5l7,7L19,9z"/></g></svg>`
+const waitingLoadSvgBlack = `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><rect fill="none" height="24" width="24"/></g><g><path d="M18,22l-0.01-6L14,12l3.99-4.01L18,2H6v6l4,4l-4,3.99V22H18z M8,7.5V4h8v3.5l-4,4L8,7.5z"/></g></svg>`
+
+const downLoadSvgWhite = `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><g><rect fill="none" height="24" width="24"/></g><g><path d="M5,20h14v-2H5V20z M19,9h-4V3H9v6H5l7,7L19,9z"/></g></svg>`
+const waitingLoadSvgWhite = `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><g><rect fill="none" height="24" width="24"/></g><g><path d="M18,22l-0.01-6L14,12l3.99-4.01L18,2H6v6l4,4l-4,3.99V22H18z M8,7.5V4h8v3.5l-4,4L8,7.5z"/></g></svg>`
+
+function displayButton() {
+  //Create Button
+  let launcher = document.createElement('button')
+  launcher.onclick = function () {
+    loadingOn()
+    setupWSS()
+  }
+  launcher.id = 'gc_downloadButton'
+  launcher.style = `cursor: pointer;
+      box-shadow: rgba(0, 0, 0, 0.2) 0px 3px 5px -2px, rgba(0, 0, 0, 0.14) 0px 1px 4px 2px, rgba(0, 0, 0, 0.12) 0px 1px 4px 1px;
+      position: fixed !important;
+      bottom: 24px !important;
+      width: 56px;
+      height: 56px;
+      right: 96px !important;
+      border-radius: 50%;
+      background-color: ${gc_hexColor};
+      z-index: 9999;
+      border: 0px;`
+  gc_iconColor == 'white' ? (launcher.innerHTML = downLoadSvgWhite) : (launcher.innerHTML = downLoadSvgBlack)
+  document.body.appendChild(launcher)
+}
+
+function loadingOn() {
+  let btn = document.getElementById('gc_downloadButton')
+  gc_iconColor == 'white' ? (btn.innerHTML = waitingLoadSvgWhite) : (btn.innerHTML = waitingLoadSvgBlack)
+}
+
+function loadingOff() {
+  let btn = document.getElementById('gc_downloadButton')
+  gc_iconColor == 'white' ? (btn.innerHTML = downLoadSvgWhite) : (btn.innerHTML = downLoadSvgBlack)
+}
